e55286395a1f8f0fc2ece21ab38c1a93a4dfeb9f
François Thiré <franth2@gmail.com>
2014-10-13 13:06:12 +0200

    modèle P0 implémenté en python
    -Modèle P0 implémenté et fonctionnel
    -Gestion des options pour lancer le programme (voir P0.py -h)

--- Python/modelisation_P0/P0.py
+++ Python/modelisation_P0/P0.py
@@ -7,63 +7,164 @@ import sys, getopt
 #check the value float
 from math import isnan, isinf
 
-#pick random coefficient for the matrix u_{i,j} using triangular law
-def picking_random_coefficient_uij(n,m,value_max):
-    return [[triangular(0,value_max) for i in range(n)] for j in range(m)]
+## #pick random coefficient for the matrix u_{i,j} using triangular law
+## def picking_random_coefficient_uij(n,value_max):
+##     #by default the third argument of triangular is (high+low)/2
+##     return [[triangular(0,value_max) for j in range(n)] for i in range(n)]
 
 
 def parse_command_line(argv):
 
     try:
-        opts, args = getopt.getopt(argv, "hM:n:",["help","maxvalue","size"])
+        opts, args = getopt.getopt(argv, "hw:vM:n:",["help","maxvalue","size","verbose","write"])
     #raise if an option is not in the list below
     except getopt.GetoptError: 
         print("Options non reconnues. Veuillez utiliser P0.py -h pour en savoir plus")
         sys.exit(2)
 
-
-    value_max = None
-    size = None
+    options=dict()
+    options['value_max']=None
+    options['size']=None
+    options['verbose']=False
+    options['write']=False
+    options['filename']=None
+    #value_max = None
+    #size = None
+    #verbose=False
+    #write the model in a file
+    #write=False
+    #filename=None
+    
     for opt, arg in opts:
         if opt in ("-h", "--help"):
-            print("P0.py -M <value max for the coef. matrix> -n <size of the problem>")
+            print("P0.py -M <value max for the coef. matrix> -n <size of the problem> [-v]")
             print("Options :")
-            print("-n <entier> : Spécifier la taille du problème")
-            print("-M <float>: Spécifier la valeur maximale pour les coefficients d'utilité")
-            print("--size : voir -n") 
-            print("--maxvalue : voir -M")
+            print("\t-n (--size) <entier> : Spécifier la taille du problème")
+            print("\t-M (--maxvalue) <float> : Spécifier la valeur maximale pour les coefficients d'utilité")
+            print("\t-v (--verbose) : affiche des informations supplémentaires à l'écran")
+            print("\t-w (--write) : écrire le modèle dans un fichier ")
+            sys.exit(0)
             
         elif opt in ("-M", "--maxvalue"):
             try:
-                value_max=float(arg)
-                if isnan(value_max) or isinf(value_max) or value_max<=0:
+                options["value_max"]=float(arg)
+                if isnan(options["value_max"]) or \
+                    isinf(options["value_max"]) or \
+                    options["value_max"]<=0:
                     raise ValueError
             except ValueError:
                 print("Erreur: la valeur maximum doit être un floattant strictement positif valide")
                 sys.exit(3)
         elif opt in ("-n", "--size"):
             try:
-                size=int(arg)
-                if size<=0:
+                options["size"]=int(arg)
+                if options["size"]<=0:
                     raise ValueError
             except ValueError:
                 print("Erreur: la taille doit être un entier strictement positif")
-
-    if size == None:
+        elif opt in("-v", "--verbose"):
+            options["verbose"]=True
+        elif opt in("-w", "--write"):
+            options["write"]=True
+            #check filename
+            options["filename"]=arg
+            
+    #if the user has not specified some parameters
+    if options["size"] == None:
         print("Attention, pas de taille spécifiée pour le problème.")
         print("Par défault, la taille vaudra 10")
-        size=10
+        options["size"]=10
 
-    if value_max == None:
+    if options["value_max"] == None:
         print("Attention, pas de taille spécifiée pour le problème.")
         print("Par défault, la valeur maximale sera 10")
-        value_max=10
+        options["value_max"]=10
+    
+    return options
 
-    return (value_max,size)
+#insert variables in the model
+def createVariables(m, options):
+
+    size=options["size"]
+    if options["verbose"]:
+        print("Add the variables to the model")
+
+    
+    #the name of the variable is "x:i:j"
+    #the variable is a binary
+    #there are size*size variables    
+    return [[m.addVar(vtype=GRB.BINARY, name=":".join(["x",str(i),str(j)])) \
+             for j in range(size)] for i in range(size)]
+
+#set the objective function
+def setObjectiveFunction(m, options, Variables):
+    size=options['size']
+    value_max=options['value_max']
+    
+    if options["verbose"]:
+        print("Set the objective function")
+    
+    for i in range(size):
+        for j in range(size):
+            #for each variable, we add its coefficient in the objective function
+            #the coefficient is a random value following a triangular law of parameter [0;M] center in M/2
+            Variables[i][j].setAttr("Obj", triangular(0,value_max))
+
+    
+    m.setAttr("ModelSense", GRB.MAXIMIZE)
+
+#add the constraints to the model
+def addConstraints(m,options, Variables):
+    size=options["size"]
+    
+    if options["verbose"]:
+        print("Add the constraints to the model")
+    
+    for i in range(size):
+        l=LinExpr() #one object per person
+        c=LinExpr() #one person per object
+        for j in range(size):
+            l.add(Variables[i][j],"1.")
+            c.add(Variables[j][i],"1.")
         
-def main(argv):
-    (value_max,size)=parse_command_line(argv)
+        m.addConstr(l,GRB.EQUAL,1., "l"+str(i))
+        m.addConstr(c,GRB.EQUAL,1., "c"+str(i))
+        
 
+#create the model P0
+def createModel(options):
+    if options["verbose"]:
+        print("Creating the model...")
+    m= Model("P0")
+    Variables=createVariables(m, options)
+    m.update()
+    setObjectiveFunction(m, options, Variables)
+    m.update()
+    addConstraints(m, options, Variables)
+    m.update()
+    return m
+
+#main function
+def main(argv):
+    options=parse_command_line(argv)
+    m=createModel(options)
+
+    #write the model in a file
+    if options["write"]:
+        m.write(filename)
+
+    if options["verbose"]:
+        print("Solving...")
+    #solve the linear problem
+    m.optimize()
+    
+    #obj=m.getObjective()
+    #print(obj)
+    #print(m.getConstrs())
+    ## for v in m.getVars():
+    ##     print v.varName, v.x
+    
+    #print 'Obj:', m.objVal
 
 #Entry point of the program
 if __name__ =="__main__":
